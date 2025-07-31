15d2b12e75684822e0db36aa601fb9951494a600
Jason Polito <jasonpolito@gmail.com>
2025-07-31 10:05:34 -0400

    deploy test

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  alert(window.outerHeight);
+</script>
+
+<div class="p-8">
+  <div
+    class="bg-slate-50 rounded shadow-inner p-8 min-h-[50dvh] flex justify-center items-center"
+  >
+    <Counter />
+  </div>
+</div>
