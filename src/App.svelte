71a8f19818c49d9f2690e22c10d0276b5d40d7b4
Jason Polito <jasonpolito@gmail.com>
2025-08-01 15:30:27 -0400

    tabs testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,21 @@
 <script>
   import Counter from "./lib/Counter.svelte";
   import { stylesObjectToCssMap } from "./import/utils";
-  import { styles } from "./import/styles";
+  import { appData } from "./import/appData";
   const STORAGE_KEY = "btnData";
 
   const initialData =
     location.search.indexOf("reset") > -1
-      ? styles
+      ? appData
       : JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  let btn = $state(initialData);
+  let app = $state(initialData);
+  console.log(app);
 
   let inlineStyles = $derived.by(() => {
-    const cssValues = stylesObjectToCssMap(btn);
+    const cssValues = stylesObjectToCssMap(app.variables);
+
+    // convert JSON object to inline style attribute string
     let styleString =
       "cursor: pointer; " +
       Object.entries(cssValues)
@@ -22,15 +25,10 @@
   });
 
   $effect(() => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(btn));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(app));
   });
 </script>
 
-<label
-  for="default-range"
-  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-  >Default range</label
->
 <div class="container mx-auto p-8 max-w-3xl">
   <div
     class="flex min-h-[50vh] items-center justify-center rounded border border-slate-100 bg-slate-50 p-16 shadow-inner"
@@ -38,20 +36,42 @@
     <button style={inlineStyles}> Howdy Partner! </button>
   </div>
 
-  <ul>
-    {#each Object.entries(btn) as [key, style]}
+  <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 my-4">
+    {#each Object.entries(app.tabs.all) as [key, tab]}
       <li>
-        <label>
-          {style.label}
-          <input
-            type={style.type}
-            class={style.type === "range"
-              ? "w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
-              : ""}
-            bind:value={style.value}
-          />
-        </label>
+        <button
+          onclick={(app.tabs.active = key)}
+          class="inline-block p-4 cursor-pointer rounded-lg {app.tabs.active ===
+          key
+            ? 'text-blue-600 bg-gray-100'
+            : 'hover:text-gray-600 hover:bg-gray-50'}">{tab.label}</button
+        >
       </li>
     {/each}
   </ul>
+
+  {#if app.tabs.active === "example"}
+    <div class="p-8 shadow-md rounded"></div>
+  {/if}
+
+  {#if app.tabs.active === "variables"}
+    <div class="p-8 shadow-md rounded">
+      <ul>
+        {#each Object.entries(app.variables) as [key, style]}
+          <li>
+            <label>
+              {style.label}
+              <input
+                type={style.type}
+                class={style.type === "range"
+                  ? "w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+                  : ""}
+                bind:value={style.value}
+              />
+            </label>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {/if}
 </div>
---
+++ src/import/appData.js
@@ -0,0 +1,50 @@
+export const appData = {
+  tabs: {
+    active: "variables",
+    all: {
+      variables: {
+        label: "vars",
+      },
+      example: {
+        label: "test",
+      },
+    },
+  },
+  variables: {
+    paddingX: {
+      value: 18,
+      label: "Horizontal Padding",
+      type: "range",
+      css: ["padding-left", "padding-right"],
+      tw: "px",
+    },
+    paddingY: {
+      value: 12,
+      label: "Vertical Padding",
+      type: "range",
+      css: ["padding-top", "padding-bottom"],
+      tw: "py",
+    },
+    color: {
+      value: "#ffffff",
+      label: "Text Color",
+      type: "color",
+      css: "color",
+      tw: "color",
+    },
+    background: {
+      value: "#1ca0ba",
+      label: "Background Color",
+      type: "color",
+      css: "background-color",
+      tw: "bg",
+    },
+    corners: {
+      value: 4,
+      label: "Corner Radius",
+      type: "range",
+      css: "border-radius",
+      tw: "rounded",
+    },
+  },
+};
